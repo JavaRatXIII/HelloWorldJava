e714887d5379ba8aca97f56e40f1668526a760f8
JavaRatXIII <tamannaqureshi9@gmail.com>
2015-08-15 17:21:39 +0100

    Initial code commit

---
+++ HelloWorld/src/helloworld/HelloWorld.java
@@ -0,0 +1,12 @@
+
+package helloworld;
+
+public class HelloWorld
+{
+    public static void main(String[] args)
+    {
+        System.out.println("Hello World");
+        System.out.println("This is Jun I hope to be uploading many more");
+        System.out.println("programs here on GitHub"); 
+    }    
+}
